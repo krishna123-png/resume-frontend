b5faaa4045282b9d92082e3189c27a09dc737ebf
krishna123-png <khirani787@gmail.com>
2025-06-18 20:50:08 +0530

    changed image paths

--- components/Home.jsx
+++ components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "react-router-dom"
 import { AuthContext } from "../src/App"
 import './Home.css'
+import image from '../'
 
 export default function Home() {
     const { user } = React.useContext(AuthContext);
@@ -22,13 +23,13 @@ export default function Home() {
                     }
                 </section>
                 <section className='image'>
-                    <img src='../../src/assets/ResumePDF.jpg' alt='logo' />
+                    <img src='../src/assets/ResumePDF.jpg' alt='logo' />
                 </section>
             </section>
             <section className='description'>
                 <section className='ai-powered'>
                     <div className="header-holder">
-                        <img src="../../src/assets/Aipowered.jpg" alt='ai-image' />
+                        <img src="../src/assets/Aipowered.jpg" alt='ai-image' />
                         <h3 className="home-h3">
                             Ai Powered
                         </h3>
@@ -39,7 +40,7 @@ export default function Home() {
                 </section>
                 <section className='pdf-download'>
                     <div className="header-holder">
-                        <img src="../../src/assets/instantPDFdownload.jpg" alt='download-icon' />
+                        <img src="../src/assets/instantPDFdownload.jpg" alt='download-icon' />
                         <h3 className="home-h3">
                             Instant PDFDownload
                         </h3>
@@ -50,7 +51,7 @@ export default function Home() {
                 </section>
                 <section className="email-integration">
                     <div className="header-holder">
-                        <img src="../../src/assets/Email-Integration.jpg" alt="email-icon" />
+                        <img src="../src/assets/Email-Integration.jpg" alt="email-icon" />
                         <h3 className="home-h3">
                             Email Integration
                         </h3>
